ad5c142ebc9ee4bb49c1ff611092173f8de941b1
Nana Adjei Manu <claeusdev@outlook.com>
2019-07-13 05:35:04 +0000

    init commit + variables

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let z;
+z = 23;
+z = "hello" // This is possible because "top types" can take any value
+
+let x: number; // type annotation
+x = 40;
+// x = "abc"  won't work because type of variable defined above
